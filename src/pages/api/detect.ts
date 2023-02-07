df0e25f6842ba7845be82eb0f3aa2542fe227d53
Frantz Kati <bahdcoder@gmail.com>
2023-02-07 14:45:03 +0100

    chore: add cors

--- src/pages/api/detect.ts
+++ src/pages/api/detect.ts
@@ -1,11 +1,19 @@
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import NextCors from 'nextjs-cors'
 import DeviceDetector from "device-detector-js"
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ })
   
   if (req.method !== 'POST') {
     return res.status(400).json({ message: "Please make a POST request with the window.navigator.userAgent value." })
